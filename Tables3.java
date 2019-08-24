0afe595ba7c6900ea3174ec9682688795bf87d32
mtsjsultan <54393374+mtsjsultan@users.noreply.github.com>
2019-08-24 18:34:59 +0530

    Add files via upload

---
+++ Tables3.java
@@ -0,0 +1,17 @@
+class Tables3
+{
+ public static void main(String[] args)
+ {
+   int tab=3,ans;
+   System.out.printf("%d*1=%d\n",tab,ans=tab*1);
+   System.out.printf("%d*2=%d\n",tab,ans=tab*2);
+   System.out.printf("%d*3=%d\n",tab,ans=tab*3);
+   System.out.printf("%d*4=%d\n",tab,ans=tab*4);
+   System.out.printf("%d*5=%d\n",tab,ans=tab*5);
+   System.out.printf("%d*6=%d\n",tab,ans=tab*6);
+   System.out.printf("%d*7=%d\n",tab,ans=tab*7);
+   System.out.printf("%d*8=%d\n",tab,ans=tab*8);
+   System.out.printf("%d*9=%d\n",tab,ans=tab*9);
+   System.out.printf("%d*10=%d\n",tab,ans=tab*10);
+  }
+}
